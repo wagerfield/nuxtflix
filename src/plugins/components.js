e776ee52f87672b711e656837a0fc3699acfc65d
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-08 10:48:08 +0000

    added components plugin

---
+++ src/plugins/components.js
@@ -0,0 +1,18 @@
+import Vue from "vue"
+import Block from "../components/block/block.vue"
+import Button from "../components/button/button.vue"
+import Container from "../components/container/container.vue"
+import Flex from "../components/flex/flex.vue"
+import Footer from "../components/footer/footer.vue"
+import Header from "../components/header/header.vue"
+import Layout from "../components/layout/layout.vue"
+import Logo from "../components/logo/logo.vue"
+
+Vue.component("v-block", Block)
+Vue.component("v-button", Button)
+Vue.component("v-container", Container)
+Vue.component("v-flex", Flex)
+Vue.component("v-footer", Footer)
+Vue.component("v-header", Header)
+Vue.component("v-layout", Layout)
+Vue.component("v-logo", Logo)
