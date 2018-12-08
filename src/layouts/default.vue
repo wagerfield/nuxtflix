7f62dfe97ccd2bc89937f0e94ddda9e11651d993
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-08 10:47:24 +0000

    added layouts

---
+++ src/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <v-header />
+    <main role="main"><nuxt /></main>
+    <v-footer />
+  </div>
+</template>
---
+++ src/layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 v-text="status" />
+    <p v-text="error.message" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    status() {
+      return this.error.statusCode || 500
+    },
+    message() {
+      return this.error.message || "Something went wrong"
+    }
+  }
+}
+</script>
