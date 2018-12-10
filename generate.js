d2388af3702ceb6e234e1f65bdb2d25dd7ec9d7f
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-10 12:16:50 +0000

    added comments

--- generate.js
+++ generate.js
@@ -1,8 +1,8 @@
 import { FILMS } from "./src/core/constants"
 
 export default {
-  fallback: true,
-  subFolders: false,
+  fallback: true, // Create 404.html
+  subFolders: false, // about/index.html > about.html
   routes() {
     return FILMS.map((film) => ({
       route: `/film/${film}`,
