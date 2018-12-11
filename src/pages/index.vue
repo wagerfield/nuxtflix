e58d2ae35aba592a3685e8fcdec00a4cb644f570
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-11 16:42:23 +0000

    added browser class

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <h1>Index</h1>
-    <button v-if="!$standalone" @click="$install">Install</button>
+    <button class="browser" @click="$install">Install</button>
   </div>
 </template>
