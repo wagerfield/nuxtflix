2c5f08ff60e3e197c30d5b1160724f8f9a9f15b5
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-11 15:52:09 +0000

    updated install plugin

--- src/plugins/install.js
+++ src/plugins/install.js
@@ -1,15 +1,21 @@
 import Vue from "vue"
 
 export default () => {
+  let installPrompt = null
+
+  Vue.prototype.$standalone = false
+
+  Vue.prototype.$install = () => {
+    console.log("install:", installPrompt)
+    if (installPrompt) installPrompt()
+  }
+
   if (process.client) {
     // Before install prompt handler
     window.addEventListener("beforeinstallprompt", (event) => {
       event.preventDefault()
       console.log("beforeinstallprompt", event)
-      Vue.prototype.$install = () => {
-        console.log("install", event)
-        event.prompt()
-      }
+      installPrompt = event.prompt
     })
 
     // App installed handler
@@ -21,9 +27,7 @@ export default () => {
     const query = "(display-mode: standalone)"
     const matches = window.matchMedia(query).matches
     Vue.prototype.$standalone = Boolean(matches)
-
-    console.log("standalone:", Vue.prototype.$standalone)
-  } else {
-    Vue.prototype.$standalone = false
   }
+
+  console.log("standalone:", Vue.prototype.$standalone)
 }
