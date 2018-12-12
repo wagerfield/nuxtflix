13c87bcf3a56b051abf93201457929224743672e
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-12 19:05:17 +0000

    added gulp script for generating webfonts

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest, series, parallel } = require("gulp")
+const plugins = require("gulp-load-plugins")()
+
+const fonts = {
+  styles: "src/styles",
+  dest: "src/static/fonts",
+  src: [
+    "assets/fonts/AlfaSlabOne-Regular.ttf",
+    "assets/fonts/RobotoMono-Regular.ttf",
+    "assets/fonts/RobotoMono-Bold.ttf"
+  ]
+}
+
+function clean() {
+  return src(["dist", "src/static/sw.js", fonts.dest], {
+    allowEmpty: true,
+    read: false
+  }).pipe(plugins.clean())
+}
+
+function convertFontsWith(converter) {
+  return function convertFonts() {
+    return src(fonts.src)
+      .pipe(converter())
+      .pipe(dest(fonts.dest))
+  }
+}
+
+const convertWebFonts = parallel(
+  convertFontsWith(plugins.ttf2woff),
+  convertFontsWith(plugins.ttf2woff2)
+)
+
+exports.clean = clean
+exports.default = series(clean, convertWebFonts)
