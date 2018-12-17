15e28505273f36c3867039b7cbabdf9a9a3c0df1
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-17 18:41:16 +0000

    removed layout wrappers

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="default-layout" :class="{ offline }">
-    <div class="upper-wrapper">
-      <div class="header-wrapper"><v-header /></div>
-      <main class="page-wrapper"><nuxt /></main>
-    </div>
-    <div class="lower-wrapper"><v-footer /></div>
+    <v-header />
+    <main><nuxt /></main>
+    <v-footer />
   </div>
 </template>
 
@@ -15,29 +13,3 @@ export default {
   computed: mapState(["offline"])
 }
 </script>
-
-<style lang="scss">
-.default-layout {
-  display: flex;
-  flex-direction: column;
-
-  .upper-wrapper {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  .header-wrapper {
-    flex: 0 0 auto;
-    z-index: 10;
-
-    @media (min-width: $max-width) {
-      height: 0;
-    }
-  }
-
-  .page-wrapper {
-    flex: 1 0 auto;
-  }
-}
-</style>
