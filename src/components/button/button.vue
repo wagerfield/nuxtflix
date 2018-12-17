d2baa360fb711dcb863eb59bd4bee2f8615f2382
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-17 19:27:39 +0000

    added basic button styles

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,5 +1,5 @@
 <template>
-  <component :is="tag"><slot /></component>
+  <component class="button" :is="tag"><slot /></component>
 </template>
 
 <script>
@@ -12,3 +12,26 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.button {
+  border: none;
+  cursor: pointer;
+  position: relative;
+  background: $white;
+  box-shadow: 0 0 0 1px $black inset;
+  padding: 0.75em 1em;
+  z-index: 1;
+
+  // &:before {
+  //   z-index: 0;
+  //   content: "";
+  //   width: 100%;
+  //   height: 100%;
+  //   background: $black;
+  //   position: absolute;
+  //   left: 0;
+  //   top: 0;
+  // }
+}
+</style>
