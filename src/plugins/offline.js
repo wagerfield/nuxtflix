df01bf671c6dd68c8565242f347bff011b3469be
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-23 09:37:33 +0000

    added setOffline initial call

--- src/plugins/offline.js
+++ src/plugins/offline.js
@@ -2,4 +2,5 @@ export default ({ store }) => {
   const setOffline = () => store.commit("setOffline", !navigator.onLine)
   window.addEventListener("offline", setOffline)
   window.addEventListener("online", setOffline)
+  setOffline()
 }
