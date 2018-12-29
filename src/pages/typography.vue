62b7a2b12cdb78928f7f2ecff0841a38716fd7ab
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-29 10:54:07 +0000

    added typography

--- src/pages/typography.vue
+++ src/pages/typography.vue
@@ -1,29 +1,45 @@
 <template>
   <section>
     <h1>Typography</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit corporis
+      earum libero unde quas dicta rerum aspernatur facere officiis debitis
+      beatae id illum culpa, laborum iure quasi atque voluptatem est temporibus
+      nisi. Maxime odit rerum facere voluptates officiis praesentium quisquam
+      architecto animi temporibus veniam. Impedit ducimus odio animi eveniet,
+      suscipit doloribus nam ad eos neque in officia amet, quod eaque.
+    </p>
     <h2>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, illum?
     </h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit corporis
+      earum libero unde quas dicta rerum aspernatur facere officiis debitis
+      beatae id illum culpa, laborum iure quasi atque voluptatem est temporibus
+      nisi. Maxime odit rerum facere voluptates officiis praesentium quisquam
+      architecto animi temporibus veniam. Impedit ducimus odio animi eveniet,
+      suscipit doloribus nam ad eos neque in officia amet, quod eaque.
+    </p>
     <h3>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum
       consequatur alias optio obcaecati est amet dolorem in esse vero
       architecto?
     </h3>
-    <p>ABCDEFGHIJKLMNOPQRSTUVWXYZ</p>
-    <p>abcdefghijklmnopqrstuvwxyz</p>
-    <p>0123456789</p>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non cum harum,
-      corporis vitae quod recusandae quisquam modi, molestiae qui eos incidunt
-      soluta reprehenderit explicabo cumque? Commodi hic vel ad officiis
-      quibusdam voluptate quasi cum iure maxime omnis eaque dignissimos
-      deleniti, in eos est rerum qui odit modi ratione rem obcaecati odio
-      explicabo, exercitationem repellat. Ipsum doloribus pariatur alias
-      laboriosam repellendus recusandae distinctio in cupiditate tempora
-      perferendis explicabo quisquam quae officiis magni vitae architecto nemo,
-      commodi cum odit assumenda! Numquam, vero ex aperiam natus, repellat
-      necessitatibus enim harum sequi suscipit amet, aspernatur esse? Quibusdam
-      neque et sapiente? Explicabo voluptas sunt soluta.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit corporis
+      earum libero unde quas dicta rerum aspernatur facere officiis debitis
+      beatae id illum culpa, laborum iure quasi atque voluptatem est temporibus
+      nisi. Maxime odit rerum facere voluptates officiis praesentium quisquam
+      architecto animi temporibus veniam. Impedit ducimus odio animi eveniet,
+      suscipit doloribus nam ad eos neque in officia amet, quod eaque.
+    </p>
+    <p class="mono">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit corporis
+      earum libero unde quas dicta rerum aspernatur facere officiis debitis
+      beatae id illum culpa, laborum iure quasi atque voluptatem est temporibus
+      nisi. Maxime odit rerum facere voluptates officiis praesentium quisquam
+      architecto animi temporibus veniam. Impedit ducimus odio animi eveniet,
+      suscipit doloribus nam ad eos neque in officia amet, quod eaque.
     </p>
   </section>
 </template>
