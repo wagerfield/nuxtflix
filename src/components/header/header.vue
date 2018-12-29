2d7aea209985756a08db6a2bc37a76bc2dbe862a
Matthew Wagerfield <matthew@wagerfield.com>
2018-12-29 10:51:54 +0000

    updated header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,8 @@
 <template>
-  <header class="global-header">
-    <nuxt-link class="global-header-root-link" to="/">Nuxtflix</nuxt-link>
+  <header class="global-header mono">
+    <nuxt-link class="global-header-nav-link" to="/" exact>
+      <span>Nuxtflix</span>
+    </nuxt-link>
     <nav class="global-header-nav">
       <ul class="global-header-nav-list">
         <li
@@ -8,11 +10,9 @@
           v-for="link in links"
           :key="link.key"
         >
-          <nuxt-link
-            class="global-header-nav-link"
-            v-text="link.text"
-            :to="link.path"
-          />
+          <nuxt-link class="global-header-nav-link" :to="link.path">
+            <span v-text="link.text" />
+          </nuxt-link>
         </li>
       </ul>
     </nav>
@@ -43,10 +43,12 @@ export default {
 
 <style lang="scss">
 .global-header {
-  background-color: $black;
   display: flex;
-  padding: 0.75rem;
+  align-items: center;
   justify-content: space-between;
+  background-color: $black;
+  height: $header-height;
+  padding: 0 1rem;
 
   .offline & {
     border-top: 4px solid $red;
@@ -57,19 +59,41 @@ export default {
   display: flex;
 }
 
-.global-header-nav-link,
-.global-header-root-link {
-  padding: 0.5rem 0.75rem;
-  display: block;
-  color: $white;
-
-  &:hover {
-    box-shadow: 0 0 0 1px $white inset;
-  }
+.global-header-nav-item + .global-header-nav-item {
+  margin-left: 0.5rem;
 }
 
-.global-header-nav-link.link-active,
-.global-header-root-link.link-active-exact {
-  color: $red;
+.global-header-nav-link {
+  padding: 0.5rem;
+  display: block;
+  line-height: 1;
+  font-size: 14px;
+  color: $white;
+
+  > span {
+    position: relative;
+    display: block;
+
+    &:after {
+      content: "";
+      opacity: 0;
+      transform: translate(0, 8px);
+      transition: all 0.2s ease-out;
+      border-bottom: 1px solid $white;
+      position: absolute;
+      display: block;
+      width: 100%;
+      bottom: 0;
+      left: 0;
+    }
+  }
+
+  &:hover,
+  &.link-active {
+    > span:after {
+      transform: translate(0, 4px);
+      opacity: 1;
+    }
+  }
 }
 </style>
