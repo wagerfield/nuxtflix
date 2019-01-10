2c3f78dbc6382f15819f6df2911baa0d4bdb8e4e
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-10 20:28:59 +0000

    removed legacy flags

--- src/plugins/webfonts.js
+++ src/plugins/webfonts.js
@@ -1,17 +1,11 @@
 import WebFont from "webfontloader"
 
 WebFont.load({
+  // https://github.com/typekit/fvd
   custom: {
-    // https://github.com/typekit/fvd
     // families: ["Noway:n7"]
   },
   google: {
     families: ["Barlow:400", "Barlow Condensed:700", "Roboto Mono:400"]
   }
-  // active() {
-  //   sessionStorage.fontsLoaded = true
-  // },
-  // inactive() {
-  //   sessionStorage.fontsLoaded = false
-  // }
 })
