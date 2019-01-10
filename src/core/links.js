3f609b2dc5cac617a502435e83eed69c1c1b2637
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-10 15:20:39 +0000

    added external links

---
+++ src/core/links.js
@@ -0,0 +1,4 @@
+export const REPOSITORY = "https://github.com/wagerfield/nuxtflix"
+export const CONTENTFUL = "https://www.contentful.com"
+export const NETLIFY = "https://www.netlify.com"
+export const NUXT = "https://nuxtjs.org"
