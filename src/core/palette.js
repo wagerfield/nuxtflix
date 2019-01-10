d10ad3be798024f1b43cfc3dd10a8dc518902cb7
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-10 16:58:47 +0000

    added palatte constants

---
+++ src/core/palette.js
@@ -0,0 +1,17 @@
+// Greys
+
+export const WHITE = "#ffffff"
+export const SMOKE = "#f3f3f5"
+export const STONE = "#e1e1e6"
+export const STEEL = "#939399"
+export const SLATE = "#49494d"
+export const NIGHT = "#303033"
+export const BLACK = "#17171a"
+
+// Colors
+
+export const RED = "#ff5b4c"
+export const YELLOW = "#ffd659"
+export const GREEN = "#45e6b0"
+export const SAGE = "#c6e5df"
+export const BLUE = "#4c92d9"
