e0eaac60c404314dd413c81e81ed7db167069c97
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-11 13:47:12 +0000

    updated hostname logic to use netlify's env variables

--- nuxt/sitemap.js
+++ nuxt/sitemap.js
@@ -1,12 +1,18 @@
 import routes from "./routes"
 
 const lastmodISO = new Date().toISOString()
+const hostname = {
+  "branch-deploy": process.env.DEPLOY_PRIME_URL,
+  "deploy-preview": process.env.DEPLOY_URL
+}
+
 const mapRoute = ({ route: url }) => ({ url, lastmodISO })
 const mapRoutes = (routes) => routes.map(mapRoute)
 
 export default {
+  // https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
+  hostname: hostname[process.env.CONTEXT] || process.env.URL,
   routes: () => routes().then(mapRoutes),
-  hostname: process.env.DEPLOY_PRIME_URL, // https://www.netlify.com/docs/continuous-deployment/#build-environment-variables
   generate: true,
   gzip: true
 }
