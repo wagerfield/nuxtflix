ad276f97e53bf32585971d3c9d9ca37a022f7263
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-15 22:25:20 +0000

    implemented button component

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,6 +1,6 @@
 <template>
-  <component :is="tag" class="button" v-on="$listeners">
-    <div class="button-content mono uppercase" v-text="text" />
+  <component :is="tag" class="button" :class="classes" v-on="$listeners">
+    <div class="button-text mono uppercase" v-text="text" />
   </component>
 </template>
 
@@ -15,43 +15,26 @@ export default {
       type: String,
       default: "Button"
     },
-    theme: {
-      type: String,
-      default: "light",
-      validate(value) {
-        return /^(light|dark)$/.test(value)
-      }
+    dark: {
+      type: Boolean
+    },
+    fill: {
+      type: Boolean
+    },
+    elevate: {
+      type: Boolean
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        this.dark && "button-dark",
+        this.fill && "button-fill",
+        this.elevate && "button-elevate"
+      ]
     }
   }
 }
 </script>
 
-<style lang="scss">
-$button-border-radius: 0px;
-$button-offset-over: 3px;
-$button-offset-out: 7px;
-
-.button {
-  padding: 0;
-  border: none;
-  cursor: pointer;
-  background: $black;
-  border-radius: $button-border-radius;
-
-  &:hover .button-content {
-    transform: translate3d($button-offset-over, -$button-offset-over, 0);
-  }
-}
-
-.button-content {
-  transition: transform 0.1s ease-out;
-  transform: translate3d($button-offset-out, -$button-offset-out, 0);
-  border-radius: $button-border-radius;
-  box-shadow: 0 0 0 2px $black inset;
-  background: $white;
-  padding: 1em 2em;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 1;
-}
-</style>
+<style lang="scss" src="./button.scss" />
