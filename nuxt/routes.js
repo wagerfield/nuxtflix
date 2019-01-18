e052893e74ec34cc27f7be8a5e7a385652341302
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-18 20:30:39 +0000

    updated route generation logic

--- nuxt/routes.js
+++ nuxt/routes.js
@@ -1,11 +1,17 @@
+import { ap } from "ramda"
 import create from "../src/cms/contentful"
 import env from "./env"
+
+const filmRoute = (path) => (film) => ({
+  route: `${path}/${film.slug}`,
+  payload: film
+})
+
+const mapFilms = ap([filmRoute("/film"), filmRoute("/watch")])
 
 export default async () => {
   const cms = create(env)
   const films = await cms.getFilms()
-  return films.map((film) => ({
-    route: `/film/${film.fields.slug}`,
-    payload: film
-  }))
+  const filmRoutes = mapFilms(films)
+  return filmRoutes
 }
