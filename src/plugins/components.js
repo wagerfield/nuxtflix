1e7278b6e70393dfa9246f29d69a49d32a112390
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-18 20:29:58 +0000

    added new components

--- src/plugins/components.js
+++ src/plugins/components.js
@@ -1,12 +1,14 @@
 import Vue from "vue"
-import Block from "~/components/block/block.vue"
 import Button from "~/components/button/button.vue"
+import Image from "~/components/image/image.vue"
+import FilmCover from "~/components/film/film-cover.vue"
 import Footer from "~/components/footer/footer.vue"
 import Header from "~/components/header/header.vue"
 import Logo from "~/components/logo/logo.vue"
 
-Vue.component("v-block", Block)
 Vue.component("v-button", Button)
+Vue.component("v-image", Image)
+Vue.component("v-film-cover", FilmCover)
 Vue.component("v-footer", Footer)
 Vue.component("v-header", Header)
 Vue.component("v-logo", Logo)
