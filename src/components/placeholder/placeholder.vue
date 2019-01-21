82f5c0cf531420eeaf2668efadff184e3ed7eb87
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-21 10:22:00 +0000

    created placeholder component

---
+++ src/components/placeholder/placeholder.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="placeholder grid">
+    <div class="content">
+      <h1 class="title uppercase" v-text="title" />
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Coming soon."
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.placeholder {
+  align-content: center;
+  padding-top: $header-height;
+  box-sizing: border-box;
+  background: $stone;
+  min-height: 100%;
+
+  .content {
+    grid-column: content;
+    margin: 1rem 0 2rem;
+  }
+
+  .title {
+    max-width: 10ch;
+    font-size: 5.5rem;
+    margin: 0;
+
+    @include above(400px) {
+      font-size: 6.5rem;
+    }
+
+    @include above(600px) {
+      font-size: 7.5rem;
+    }
+  }
+}
+</style>
