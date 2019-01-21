f84f1236fe20577cef4c31acc85f49662d458ad4
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-21 14:58:38 +0000

    created basic youtube video component

---
+++ src/components/video/video.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="video">
+    <iframe class="video-iframe" :alt="alt" :src="src" allowfullscreen />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    embed: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    src() {
+      const base = "https://www.youtube.com/embed/"
+      const query = "?iv_load_policy=3&modestbranding=1"
+      return `${base}${this.embed}${query}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.video-iframe {
+  display: block;
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
