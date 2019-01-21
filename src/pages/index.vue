566f395b39e2de82f15970f4e187f5201551288c
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-21 15:15:15 +0000

    added callout wrapper

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,8 @@
 <template>
   <v-placeholder>
-    <v-button
-      v-if="install"
-      class="callout"
-      text="Install"
-      elevate
-      @click="$install"
-    />
+    <div v-if="install" class="callout">
+      <v-button text="Install" elevate @click="$install" />
+    </div>
   </v-placeholder>
 </template>
 
@@ -20,6 +16,6 @@ export default {
 
 <style lang="scss" scoped>
 .callout {
-  margin-top: 2rem;
+  margin-top: 4rem;
 }
 </style>
