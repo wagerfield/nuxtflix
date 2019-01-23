a18cd24a4a19cbf4943ef57c431ec177b5ddebd3
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-23 17:46:58 +0000

    set lazysizes expand to 0

--- src/plugins/lazysizes.js
+++ src/plugins/lazysizes.js
@@ -2,5 +2,5 @@ import lazysizes from "lazysizes"
 
 // https://github.com/aFarkas/lazysizes
 Object.assign(lazysizes.cfg, {
-  // expand: 0 // Load nearby elements before they are visible
+  expand: 0 // Load nearby elements before they are visible
 })
