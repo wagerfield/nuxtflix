8218fcc1c3cbacbd958b55be8854fafa9fd5ecf7
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-24 09:53:53 +0000

    added expand query parsing

--- src/plugins/lazysizes.js
+++ src/plugins/lazysizes.js
@@ -1,11 +1,11 @@
 import lazysizes from "lazysizes"
 
 export default ({ query }) => {
-  const expand = query.expand
+  const expand = parseInt(query.expand, 10) || 400
   // eslint-disable-next-line
   console.log("lazysizes > expand:", expand)
   // https://github.com/aFarkas/lazysizes
   Object.assign(lazysizes.cfg, {
-    expand // Load nearby elements before they are visible
+    // expand // Load nearby elements before they are visible
   })
 }
