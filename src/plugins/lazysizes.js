07a17d302b60e1c5eaa236e8876d4b424a43667e
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-25 16:07:40 +0000

    added object-fit and parent-fit plugins

--- src/plugins/lazysizes.js
+++ src/plugins/lazysizes.js
@@ -1,5 +1,7 @@
 import "lazysizes/plugins/respimg/ls.respimg"
 import "lazysizes/plugins/optimumx/ls.optimumx"
+import "lazysizes/plugins/object-fit/ls.object-fit"
+import "lazysizes/plugins/parent-fit/ls.parent-fit"
 import lazysizes from "lazysizes"
 
 export default ({ query }) => {
