2c9ec496162909547416c1cc83a4235c62246c68
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-26 10:35:47 +0000

    removed header, footer and logo

--- src/plugins/components.js
+++ src/plugins/components.js
@@ -2,17 +2,11 @@ import Vue from "vue"
 import Button from "~/components/button/button.vue"
 import Image from "~/components/image/image.vue"
 import FilmCover from "~/components/film/film-cover.vue"
-import Footer from "~/components/footer/footer.vue"
-import Header from "~/components/header/header.vue"
-import Logo from "~/components/logo/logo.vue"
 import Placeholder from "~/components/placeholder/placeholder.vue"
 import Video from "~/components/video/video.vue"
 
 Vue.component("v-button", Button)
 Vue.component("v-image", Image)
 Vue.component("v-film-cover", FilmCover)
-Vue.component("v-footer", Footer)
-Vue.component("v-header", Header)
-Vue.component("v-logo", Logo)
 Vue.component("v-placeholder", Placeholder)
 Vue.component("v-video", Video)
