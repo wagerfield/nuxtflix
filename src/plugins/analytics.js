4cc4326261ca55435e8ce15078341e547ab1687b
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-28 16:50:16 +0000

    created analytics plugin

---
+++ src/plugins/analytics.js
@@ -0,0 +1,22 @@
+import Vue from "vue"
+import VueAnalytics from "vue-analytics"
+
+// Custom Dimensions
+const BRANCH = "dimension1"
+
+export default ({ app, env, isDev }) => {
+  // https://www.npmjs.com/package/vue-analytics
+  Vue.use(VueAnalytics, {
+    id: env.GOOGLE_ANALYTICS_ID,
+    router: app.router,
+    autoTracking: {
+      exception: true,
+      screenview: true
+    },
+    set: [{ field: BRANCH, value: env.BRANCH }],
+    debug: {
+      // enabled: isDev,
+      sendHitTask: !isDev
+    }
+  })
+}
