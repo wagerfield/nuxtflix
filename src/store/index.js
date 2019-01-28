1427fcc649741fac36409923a07605340f6bd0dd
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-28 16:53:16 +0000

    added web app installer logic and analytics tracking

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
+import { event as sendEvent } from "vue-analytics"
 import { prop, propEq } from "ramda"
 
 export default {
   state: () => ({
-    install: false,
+    installer: null,
     offline: false,
     outline: false,
     films: null
   }),
   mutations: {
-    setInstall(state, install) {
-      state.install = install
+    setInstaller(state, installer) {
+      state.installer = installer
     },
     setOffline(state, offline) {
       state.offline = offline
@@ -18,10 +19,14 @@ export default {
       state.outline = outline
     },
     setFilms(state, films) {
-      state.films = films
+      // console.log("store.setFilms")
+      state.films = Object.freeze(films)
     }
   },
   getters: {
+    canInstallWebApp({ installer }) {
+      return Boolean(installer)
+    },
     filmSlugs({ films }) {
       return films && films.map(prop("slug"))
     },
@@ -30,7 +35,21 @@ export default {
     }
   },
   actions: {
+    async nuxtServerInit({ dispatch }) {
+      // console.log("store.nuxtServerInit")
+      // await dispatch("getFilms")
+    },
+    async installWebApp({ state }) {
+      const { installer } = state
+      if (installer) {
+        installer.prompt()
+        sendEvent("WebApp", "install", "User prompted to install")
+        const outcome = await installer.userChoice
+        sendEvent("WebApp", "install", `User ${outcome} prompt`)
+      }
+    },
     async getFilms({ state, commit }) {
+      // console.log("store.getFilms")
       if (!state.films) {
         commit("setFilms", await this.$cms.getFilms())
       }
