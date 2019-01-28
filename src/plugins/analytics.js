7d832a89226e16847a0b5f6653db4fd8f9f9e91c
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-28 20:26:04 +0000

    simplified config

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -2,21 +2,21 @@ import Vue from "vue"
 import VueAnalytics from "vue-analytics"
 
 // Custom Dimensions
-const BRANCH = "dimension1"
+// const BRANCH = "dimension1"
 
 export default ({ app, env, isDev }) => {
   // https://www.npmjs.com/package/vue-analytics
   Vue.use(VueAnalytics, {
     id: env.GOOGLE_ANALYTICS_ID,
-    router: app.router,
-    autoTracking: {
-      exception: true,
-      screenview: true
-    },
-    debug: {
-      // enabled: isDev,
-      sendHitTask: !isDev
-    },
-    set: [{ field: BRANCH, value: env.BRANCH }]
+    router: app.router
+    // autoTracking: {
+    //   exception: true,
+    //   screenview: true
+    // },
+    // debug: {
+    //   // enabled: isDev,
+    //   sendHitTask: !isDev
+    // },
+    // set: [{ field: BRANCH, value: env.BRANCH }]
   })
 }
