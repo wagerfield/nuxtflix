5786cb81cbe5a3706fcb5d7728ffedc28efc3e57
Matthew Wagerfield <matthew@wagerfield.com>
2019-01-29 17:34:19 +0000

    updated plugins to use mode naming convention

--- src/plugins/offline.js
+++
@@ -1,6 +0,0 @@
-export default ({ store }) => {
-  const setOffline = () => store.commit("setOffline", !navigator.onLine)
-  window.addEventListener("offline", setOffline)
-  window.addEventListener("online", setOffline)
-  setOffline()
-}
